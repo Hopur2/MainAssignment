53d20ac59922177422db823f9874d0930434361f
Alexander Gudmuundsson <alexandergud18@gmail.com>
2017-11-30 15:57:54 +0000

    Nadi ad lata 1 pontun sjast, aetla ad bua til classa fyrir pantanir

--- include/Makeline.h
+++ include/Makeline.h
@@ -1,6 +1,9 @@
 #ifndef MAKELINE_H
 #define MAKELINE_H
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,10 +11,15 @@ class Makeline
 {
     public:
         Makeline();
+        void Getorder();
+        void Seeorder();
+        void OrderSplitter();
 
     protected:
 
     private:
+        string _linereader;
+        string Order[9];
 };
 
 #endif // MAKELINE_H
--- src/Makeline.cpp
+++ src/Makeline.cpp
@@ -3,5 +3,46 @@
 Makeline::Makeline()
 {
     //ctor
-    cout << "1. Skoğa pantanir" << endl;
+    Getorder();
+    OrderSplitter();
+    Seeorder();
+}
+
+void Makeline::Getorder()
+{
+    ifstream fin;
+    fin.open("Orders.txt");
+    if(fin.is_open())
+    {
+        getline(fin, _linereader);
+    }
+    else
+    {
+        cout << "File could not be opened";
+    }
+}
+
+void Makeline::Seeorder()
+{
+    cout << "ID: " << Order[0] << "   ";
+    cout << "Size: " << Order[3] << "   ";
+    cout << "Toppings: " << Order[4] << "   ";
+    cout << "Comments: " << Order[5] << "   ";
+    cout << "Status: " << Order[7] << "   ";
+}
+
+void Makeline::OrderSplitter()
+{
+    int counter = 0;
+    for(int i = 0; i < _linereader.size(); i++)
+    {
+        if(_linereader[i] == '|')
+        {
+            counter++;
+        }
+        else
+        {
+            Order[counter] += _linereader[i];
+        }
+    }
 }
