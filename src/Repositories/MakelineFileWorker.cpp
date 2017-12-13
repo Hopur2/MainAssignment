ad521e888d64b946b6f1260122fd23c50f9c4b9a
Viktor Ingi Kárason <vingik@gmail.com>
2017-12-13 15:53:27 +0000

    lagað file vandamal

--- src/Repositories/MakelineFileWorker.cpp
+++ src/Repositories/MakelineFileWorker.cpp
@@ -9,7 +9,8 @@ void MakelineFileWorker::ReadFile(string file)
 {
     string line;
     ifstream fin;
-    fin.open(file);
+    fin.open(file.c_str());
+    //fin.open("Orders.txt");
     if(fin.is_open())
     {
         while(!fin.eof())
@@ -52,7 +53,7 @@ vector<Order> MakelineFileWorker::PassOrders() {
 void MakelineFileWorker::WriteOrders(string file1, string file2, vector<Order> Orders, vector<Order> DoneOrders)
 {
     ofstream fout;
-    fout.open(file1);
+    fout.open(file1.c_str());
     if(fout.is_open())
     {
         for(unsigned int i = 0; i < Orders.size(); i++)
@@ -67,7 +68,7 @@ void MakelineFileWorker::WriteOrders(string file1, string file2, vector<Order> O
     {
         cout << "File couldn't be opened" << endl;
     }
-    fout.open(file2, ios::app);
+    fout.open(file2.c_str(), ios::app);
     if(fout.is_open())
     {
         for(unsigned int i = 0; i < DoneOrders.size(); i++)
